64114661d75f09fbf849ab61fc76997ba56f70cb
Daniel Gadd <daniel@auraredeye.com>
2014-10-28 11:38:14 +1300

    Fixed up indentation

--- cfddns.py
+++ cfddns.py
@@ -25,72 +25,71 @@ import urllib
 
 ''' Make a request to the CloudFlare API '''
 def api_request(action, **kwargs):
-	data = {
-		'tkn': config['apikey'],
-		'email': config['email'],
-		'a': action
-	}
-	data = dict(data.items() + kwargs.items())
-	
-	r = requests.post('https://www.cloudflare.com/api_json.html', data=data)
-	
-	# Some requests versions use r.json and some use r.json()
-	json = r.json
-	try:
-		json.__getitem__
-	except AttributeError:
-		json = r.json()
-	
-	if json['result'] != 'success':
-		raise Exception(json['msg'])
-	else:
-		return json['response']
+    data = {
+        'tkn': config['apikey'],
+        'email': config['email'],
+        'a': action
+    }
+data = dict(data.items() + kwargs.items())
+r = requests.post('https://www.cloudflare.com/api_json.html', data=data)
+
+# Some requests versions use r.json and some use r.json()
+    json = r.json
+    try:
+        json.__getitem__
+    except AttributeError:
+        json = r.json()
+
+if json['result'] != 'success':
+    raise Exception(json['msg'])
+    else:
+        return json['response']
 
 ''' Get the machine's external IP '''
 def external_ip():
-	try:
-		r = urllib.urlopen("http://ipv4.icanhazip.com/").read()
-		return r.strip()
-	except:
-		raise Exception("Error getting IP")
+    try:
+        r = urllib.urlopen("http://ipv4.icanhazip.com/").read()
+    return r.strip()
+    except:
+        raise Exception("Error getting IP")
 
 def main():
-	# Get the configurations from the config file
-	globals()['config'] = {}
-	execfile(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.py'), config)
+# Get the configurations from the config file
+    globals()['config'] = {}
+    execfile(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.py'), config)
 
-	# Set up logging
-	logging.basicConfig(
-		format='%(asctime)s [%(levelname)s] %(message)s',
-		datefmt='%Y/%m/%d %H:%M:%S',
-		level=getattr(logging, config['loglevel']))
+# Set up logging
+    logging.basicConfig(
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    datefmt='%Y/%m/%d %H:%M:%S',
+    level=getattr(logging, config['loglevel']))
 
-	# Disable requests INFO messages
-	logging.getLogger('requests').setLevel(logging.WARNING)
-	
-	# Get the machine's external IP
-	config['ext_ip'] = external_ip()
+# Disable requests INFO messages
+    logging.getLogger('requests').setLevel(logging.WARNING)
 
-	logging.info('Requesting list of records...')
-	# Get the list of records
-	records = api_request('rec_load_all', z = config['zone'])['recs']['objs']
-	
-	# Iterate over every records, and change the specified ones
-	for record in records:
-		if (record['type'] == 'A') and (record['name'] in config['domains']):
-			if  (record['content'] == config['ext_ip']) and (record['ttl'] == str(config['ttl'])):
-				# Change is not necessary - The IP and config['ttl'] are good.
-				logging.info('%s doesn\'t need any change.' % record['name'])
-			else:
-				# Change is necessary
-				logging.info('Changing record %s...' % record['name'])
-				api_request('rec_edit',
-					z = config['zone'],
-					id = record['rec_id'],
-					type = record['type'],
-					name = record['name'],
-					ttl = config['ttl'],
-					content = config['ext_ip'])
+# Get the machine's external IP
+    config['ext_ip'] = external_ip()
+    logging.info('Requesting list of records...')
+
+# Get the list of records
+    records = api_request('rec_load_all', z = config['zone'])['recs']['objs']
+
+# Iterate over every records, and change the specified ones
+    for record in records:
+    if (record['type'] == 'A') and (record['name'] in config['domains']):
+        if  (record['content'] == config['ext_ip']) and (record['ttl'] == str(config['ttl'])):
+        # Change is not necessary - The IP and config['ttl'] are good.
+            logging.info('%s doesn\'t need any change.' % record['name'])
+        else:
+    # Change is necessary
+        logging.info('Changing record %s...' % record['name'])
+        api_request('rec_edit',
+        z = config['zone'],
+        id = record['rec_id'],
+        type = record['type'],
+        name = record['name'],
+        ttl = config['ttl'],
+        content = config['ext_ip'])
 
 if __name__ == '__main__':
-	main()
+    main()
